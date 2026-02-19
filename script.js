c6a2c0a5158f22dc47cf344bd472530c6cb759a1
hihumikan <a@qqey.net>
2026-02-19 13:44:24 +0900

    初回のHTML、CSS、JavaScriptファイルを追加し、全角かなと半角かなの変換機能を実装

---
+++ script.js
@@ -0,0 +1,120 @@
+const fullInput = document.getElementById("fullInput");
+const halfInput = document.getElementById("halfInput");
+
+const fullToHalfMap = {
+  "。": "｡",
+  "「": "｢",
+  "」": "｣",
+  "、": "､",
+  "・": "･",
+  "ー": "ｰ",
+  "ヲ": "ｦ",
+  "ァ": "ｧ",
+  "ィ": "ｨ",
+  "ゥ": "ｩ",
+  "ェ": "ｪ",
+  "ォ": "ｫ",
+  "ャ": "ｬ",
+  "ュ": "ｭ",
+  "ョ": "ｮ",
+  "ッ": "ｯ",
+  "ア": "ｱ",
+  "イ": "ｲ",
+  "ウ": "ｳ",
+  "エ": "ｴ",
+  "オ": "ｵ",
+  "カ": "ｶ",
+  "キ": "ｷ",
+  "ク": "ｸ",
+  "ケ": "ｹ",
+  "コ": "ｺ",
+  "サ": "ｻ",
+  "シ": "ｼ",
+  "ス": "ｽ",
+  "セ": "ｾ",
+  "ソ": "ｿ",
+  "タ": "ﾀ",
+  "チ": "ﾁ",
+  "ツ": "ﾂ",
+  "テ": "ﾃ",
+  "ト": "ﾄ",
+  "ナ": "ﾅ",
+  "ニ": "ﾆ",
+  "ヌ": "ﾇ",
+  "ネ": "ﾈ",
+  "ノ": "ﾉ",
+  "ハ": "ﾊ",
+  "ヒ": "ﾋ",
+  "フ": "ﾌ",
+  "ヘ": "ﾍ",
+  "ホ": "ﾎ",
+  "マ": "ﾏ",
+  "ミ": "ﾐ",
+  "ム": "ﾑ",
+  "メ": "ﾒ",
+  "モ": "ﾓ",
+  "ヤ": "ﾔ",
+  "ユ": "ﾕ",
+  "ヨ": "ﾖ",
+  "ラ": "ﾗ",
+  "リ": "ﾘ",
+  "ル": "ﾙ",
+  "レ": "ﾚ",
+  "ロ": "ﾛ",
+  "ワ": "ﾜ",
+  "ン": "ﾝ",
+  "ヵ": "ｶ",
+  "ヶ": "ｹ",
+  "ヮ": "ﾜ",
+  "ヰ": "ｲ",
+  "ヱ": "ｴ",
+  "゛": "ﾞ",
+  "゜": "ﾟ"
+};
+
+function toKatakana(text) {
+  return text.replace(/[\u3041-\u309f]/g, (char) => {
+    const code = char.codePointAt(0);
+    return String.fromCodePoint(code + 0x60);
+  });
+}
+
+function convertFullToHalf(text) {
+  const katakana = toKatakana(text);
+  let result = "";
+
+  for (const char of katakana) {
+    const decomposed = char.normalize("NFD");
+
+    for (const part of decomposed) {
+      if (part === "\u3099" || part === "゛") {
+        result += "ﾞ";
+        continue;
+      }
+      if (part === "\u309a" || part === "゜") {
+        result += "ﾟ";
+        continue;
+      }
+      result += fullToHalfMap[part] ?? part;
+    }
+  }
+
+  return result;
+}
+
+function convertHalfToFull(text) {
+  return text.replace(/[｡-ﾟ]+/g, (segment) => segment.normalize("NFKC"));
+}
+
+function syncFromFull() {
+  halfInput.value = convertFullToHalf(fullInput.value);
+}
+
+function syncFromHalf() {
+  fullInput.value = convertHalfToFull(halfInput.value);
+}
+
+fullInput.addEventListener("input", syncFromFull);
+halfInput.addEventListener("input", syncFromHalf);
+
+syncFromFull();
